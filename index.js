0882dabbc0a684da037ad2cb5b9af3a8624e1c74
anikobakhidze <kobakhidzeana23@gmail.com>
2024-01-26 17:00:55 +0400

    fix overflow

--- index.js
+++ index.js
@@ -6,6 +6,97 @@ const handleScroll = () => {
     : header.classList.remove("transparent");
 };
 window.addEventListener("scroll", handleScroll);
+
+// partners slider
+const rightArrow = document.getElementById("rightArrow");
+const leftArrow = document.getElementById("leftArrow");
+const partnersLists = document.querySelectorAll(".partners-list-wrapper");
+const dotSlider = document.querySelectorAll(".dot");
+let currentIndex = 0;
+
+const showCurrentList = () => {
+  partnersLists.forEach((partnerList, index) => {
+    if (index === currentIndex) {
+      partnerList.classList.remove("d-none");
+      if (dotSlider[index]) {
+        dotSlider[index].classList.add("dot-selected");
+      }
+    } else {
+      partnerList.classList.add("d-none");
+      if (dotSlider[index]) {
+        dotSlider[index].classList.remove("dot-selected");
+      }
+    }
+  });
+};
+
+const handleArrow = (direction) => {
+  currentIndex =
+    (direction === "right"
+      ? currentIndex + 1
+      : currentIndex - 1 + partnersLists.length) % partnersLists.length;
+  showCurrentList();
+};
+
+// Arrow event listeners
+rightArrow.addEventListener("click", () => handleArrow("right"));
+leftArrow.addEventListener("click", () => handleArrow("left"));
+
+// Automatic scroll
+let intervalId = setInterval(() => handleArrow("right"), 5000);
+
+// Mouse enter/leave event listeners
+const handleMouseEnter = () => clearInterval(intervalId);
+const handleMouseLeave = () =>
+  (intervalId = setInterval(() => handleArrow("right"), 5000));
+
+rightArrow.addEventListener("mouseenter", handleMouseEnter);
+rightArrow.addEventListener("mouseleave", handleMouseLeave);
+leftArrow.addEventListener("mouseenter", handleMouseEnter);
+leftArrow.addEventListener("mouseleave", handleMouseLeave);
+
+// Dot slider logic
+const dotContainer = document.querySelector(".slider-dots-container");
+
+const handleDotSlider = (e) => {
+  const currentDot = e.target.closest("li");
+  if (!currentDot) return;
+  dotSlider.forEach((slider, index) => {
+    const isCurrentDot = slider === currentDot;
+    partnersLists[index].classList.toggle("d-none", !isCurrentDot);
+    currentDot.classList.toggle("dot-selected", isCurrentDot);
+    slider.classList.toggle("dot-selected", isCurrentDot);
+  });
+};
+
+dotContainer.addEventListener("click", handleDotSlider);
+
+// Accordion card
+
+const accordionWrapper = document.querySelector(".accordion-div-wrapper");
+const accordionCards = document.querySelectorAll(".accordion-card-li");
+
+const handleClick = (e) => {
+  const closestAccordionCard = e.target.closest("li");
+  accordionCards.forEach((accordionCard) => {
+    const downArrow = accordionCard.querySelector(".down-arrow");
+    const upArrow = accordionCard.querySelector(".up-arrow");
+    const accordionAnswers = accordionCard.querySelector(
+      ".query-accordion-answer"
+    );
+    if (accordionCard === closestAccordionCard) {
+      toogleClasslist(downArrow, "d-none");
+      toogleClasslist(upArrow, "d-none");
+      toogleClasslist(accordionAnswers, "d-none");
+    } else {
+      downArrow.classList.remove("d-none");
+      upArrow.classList.add("d-none");
+      accordionAnswers.classList.add("d-none");
+    }
+  });
+};
+
+accordionWrapper.addEventListener("click", handleClick);
 // burger menu appear/disappear while clicking on burger button
 const burgerMenu = document.querySelector(".burger-bar-wrapper");
 const aside = document.querySelector(".aside");
@@ -41,10 +132,13 @@ const toggleMenu = () => {
   toogleClasslist(headerNavContainer, "nav-container-clicked");
   toogleClasslist(headerNav, "nav-wrapper-clicked");
   toogleClasslist(headerNav, "nav-wrapper");
+  isMenuOpen
+    ? clearInterval(intervalId)
+    : (intervalId = setInterval(handleArrow("right"), 5000));
 };
 burgerMenu.addEventListener("click", toggleMenu);
-// close aside menu bar
-window.addEventListener("click", (e) => {
+// close burger menu bar
+const closeBurgerMenuBar = (e) => {
   if (
     isMenuOpen &&
     e.target !== aside &&
@@ -54,13 +148,22 @@ window.addEventListener("click", (e) => {
   ) {
     toggleMenu();
   }
-});
-
-// aside section height
-window.onload = () => {
-  const bodyHeight = document.body.scrollHeight;
-  asideOverlay.style.height = bodyHeight - header.style.height + "px";
 };
+window.addEventListener("click", closeBurgerMenuBar);
+
+// burger menu bar section height
+const updateOverlayHeight = () => {
+  const bodyHeight = document.body.scrollHeight;
+  const asideHeight = aside.offsetHeight;
+  asideOverlay.style.height = bodyHeight - asideHeight + "px";
+};
+
+window.onload = () => {
+  updateOverlayHeight();
+  // Update overlay height on window scroll
+  window.addEventListener("scroll", updateOverlayHeight);
+};
+
 // Burger Menu disappears while resizing
 const handleResize = () => {
   const windowWidth = window.innerWidth;
@@ -70,98 +173,3 @@ const handleResize = () => {
 };
 
 window.addEventListener("resize", handleResize);
-
-// partners slider
-const rightArrow = document.getElementById("rightArrow");
-const leftArrow = document.getElementById("leftArrow");
-const partnersLists = document.querySelectorAll(".partners-list-wrapper");
-const dotSlider = document.querySelectorAll(".dot");
-let currentIndex = 0;
-
-const showCurrentList = () => {
-  partnersLists.forEach((el, index) => {
-    if (index === currentIndex) {
-      el.classList.remove("d-none");
-      if (dotSlider[index]) {
-        dotSlider[index].classList.add("dot-selected");
-      }
-    } else {
-      el.classList.add("d-none");
-      if (dotSlider[index]) {
-        dotSlider[index].classList.remove("dot-selected");
-      }
-    }
-  });
-};
-// dot slider
-const dotContainer = document.querySelector(".slider-dots-container");
-
-const handleDotSlider = (e) => {
-  const currentDot = e.target.closest("li");
-  if (!currentDot) return;
-  dotSlider.forEach((slider, index) => {
-    if (slider === currentDot) {
-      if (partnersLists[index].classList.contains("d-none"))
-        partnersLists[index].classList.remove("d-none");
-      currentDot.classList.add("dot-selected");
-    } else {
-      partnersLists[index].classList.add("d-none");
-      slider.classList.remove("dot-selected");
-    }
-  });
-};
-dotContainer.addEventListener("click", handleDotSlider);
-// rightArrow
-const handleRightArrow = () => {
-  currentIndex = (currentIndex + 1) % partnersLists.length;
-  showCurrentList();
-};
-rightArrow.addEventListener("click", handleRightArrow);
-// automatic scroll
-let intervalId = setInterval(handleRightArrow, 5000);
-rightArrow.addEventListener("mouseenter", () => clearInterval(intervalId));
-rightArrow.addEventListener(
-  "mouseleave",
-  () => (intervalId = setInterval(handleRightArrow, 5000))
-);
-
-leftArrow.addEventListener("mouseenter", () => clearInterval(intervalId));
-leftArrow.addEventListener(
-  "mouseleave",
-  () => (intervalId = setInterval(handleRightArrow, 5000))
-);
-
-// leftArrow
-const handleLeftArrow = () => {
-  currentIndex =
-    (currentIndex - 1 + partnersLists.length) % partnersLists.length;
-  showCurrentList();
-};
-leftArrow.addEventListener("click", handleLeftArrow);
-
-// Accordion card
-
-const accordionWrapper = document.querySelector(".accordion-div-wrapper");
-const accordionCards = document.querySelectorAll(".accordion-card-li");
-
-const handleClick = (e) => {
-  const closestAccordionCard = e.target.closest("li");
-  accordionCards.forEach((accordionCard) => {
-    const downArrow = accordionCard.querySelector(".down-arrow");
-    const upArrow = accordionCard.querySelector(".up-arrow");
-    const accordionAnswers = accordionCard.querySelector(
-      ".query-accordion-answer"
-    );
-    if (accordionCard === closestAccordionCard) {
-      toogleClasslist(downArrow, "d-none");
-      toogleClasslist(upArrow, "d-none");
-      toogleClasslist(accordionAnswers, "d-none");
-    } else {
-      downArrow.classList.remove("d-none");
-      upArrow.classList.add("d-none");
-      accordionAnswers.classList.add("d-none");
-    }
-  });
-};
-
-accordionWrapper.addEventListener("click", handleClick);
